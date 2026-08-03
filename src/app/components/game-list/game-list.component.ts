cb26a534e8dc2241bc88d8e444072ec17ab9e1b7
mikizuk <mikizuk@gmail.com>
2026-08-03 21:57:58 +0200

    Added modern styles

--- src/app/components/game-list/game-list.component.ts
+++ src/app/components/game-list/game-list.component.ts
@@ -2,6 +2,10 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { IndieGamesService } from './../../service/indie-games.service';
 import { Game } from '../../types/indie-games';
 
+type SortField = 'title' | 'author';
+type SortDirection = 'asc' | 'desc';
+type ViewMode = 'list' | 'grid';
+
 @Component({
   selector: 'app-game-list',
   standalone: false,
@@ -11,17 +15,49 @@ import { Game } from '../../types/indie-games';
 })
 export class GameListComponent implements OnInit {
   games: Game[] = [];
+  sortedGames: Game[] = [];
+  sortField: SortField = 'title';
+  sortDirection: SortDirection = 'asc';
+  viewMode: ViewMode = 'grid';
 
   constructor(private gamesService: IndieGamesService) {}
 
   ngOnInit(): void {
     this.gamesService.games$.subscribe({
-      next: (games: Game[]) => (this.games = games),
+      next: (games: Game[]) => {
+        this.games = games;
+        this.applySort();
+      },
       error: (err) => console.log(err),
     });
   }
 
   addSuggestedClick = (): void => {
     this.gamesService.addSuggestedGames();
+  };
+
+  onSortFieldChange = (field: SortField): void => {
+    this.sortField = field;
+    this.applySort();
+  };
+
+  onSortDirectionToggle = (): void => {
+    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    this.applySort();
+  };
+
+  onViewModeChange = (mode: ViewMode): void => {
+    this.viewMode = mode;
+  };
+
+  private applySort = (): void => {
+    const dir = this.sortDirection === 'asc' ? 1 : -1;
+    this.sortedGames = [...this.games].sort((a, b) => {
+      const av = (a[this.sortField] || '').toLowerCase();
+      const bv = (b[this.sortField] || '').toLowerCase();
+      if (av < bv) return -1 * dir;
+      if (av > bv) return 1 * dir;
+      return 0;
+    });
   };
 }
